5dcc9eaf17e5e6d69f32aebf91895912ad420ef7
Faur Ioan-Aurel <fioan89@gmail.com>
2014-06-20 22:06:45 +0300

    Adding utilities for the soon to come features. Some unit tests too.

---
+++ src/main/java/org/wavescale/hotload/agent/util/ClassUtil.java
@@ -0,0 +1,25 @@
+package org.wavescale.hotload.agent.util;
+
+/**
+ * ****************************************************************************
+ * Copyright (c) 2005-2014 Faur Ioan-Aurel.                                     *
+ * All rights reserved. This program and the accompanying materials             *
+ * are made available under the terms of the MIT License                        *
+ * which accompanies this distribution, and is available at                     *
+ * http://opensource.org/licenses/MIT                                           *
+ * *
+ * For any issues or questions send an email at: fioan89@gmail.com              *
+ * *****************************************************************************
+ */
+public class ClassUtil {
+
+    /**
+     * Replaces every "/" chararcter with ".".
+     * @param className a {@link String} representing the class name. Usually the value
+     *                  has the following form: domain.package.class
+     * @return a {@link String} in which the "/" char is replaced with "."
+     */
+    public static String normalizeClassName(String className) {
+        return className.replace("/", ".");
+    }
+}
---
+++ src/test/java/org/wavescale/hotload/util/ClassUtilTest.java
@@ -0,0 +1,31 @@
+package org.wavescale.hotload.util;
+
+import junit.framework.TestCase;
+import org.wavescale.hotload.agent.util.ClassUtil;
+
+/**
+ * ****************************************************************************
+ * Copyright (c) 2005-2014 Faur Ioan-Aurel.                                     *
+ * All rights reserved. This program and the accompanying materials             *
+ * are made available under the terms of the MIT License                        *
+ * which accompanies this distribution, and is available at                     *
+ * http://opensource.org/licenses/MIT                                           *
+ * *
+ * For any issues or questions send an email at: fioan89@gmail.com              *
+ * *****************************************************************************
+ */
+
+public class ClassUtilTest extends TestCase {
+
+    public void testNormalizeClassNameWithForwardSlash() {
+        assertEquals(ClassUtil.normalizeClassName("org/wavescale/hotload/agent.Class"), "org.wavescale.hotload.agent.Class");
+    }
+
+    public void testNormalizeClassNameWithPeriod() {
+        assertEquals(ClassUtil.normalizeClassName("org.wavescale.hotload.agent.Class"), "org.wavescale.hotload.agent.Class");
+    }
+
+    public void testNormalizeClassWithMixedChars() {
+        assertEquals(ClassUtil.normalizeClassName("org/wavescale/hotload.agent.Class"), "org.wavescale.hotload.agent.Class");
+    }
+}
